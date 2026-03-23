2131c43b9dcc5490c776fdfd3a8a74b95dd4268b
joparrab-unal <joparrab@unal.edu.co>
2026-03-23 10:55:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,13 +16,38 @@ public class Main {
 
     public static void main(String[] args) {
         final int start = 100;
-        final int end = 10000;
+        final int endLento = 10000;
+        final int endRapido = 1000000
         
-        for (int size = start; size <= end; size *= 10)
-            for (int i =0; i<5; i++){
-                ListaSimple<Integer> lista = new ListaSimple<>();
-                exec(size, "pushFront", lista::pushFront;
-            }
-    
+       for (int size = start; size <= endRapido; size *= 10) {
+    for (int j = 0; j < 5; j++) {
+        ListaSimple<Integer> lista = new ListaSimple<>();
+        exec(size, "pushFront", lista::pushFront);
+    }
+}
+        for (int size = start; size <= endRapido; size *= 10) {
+    for (int j = 0; j < 5; j++) {
+        ListaSimple<Integer> lista = new ListaSimple<>();
+        exec(size, "pushBack", lista::pushBack);
+    }
+}
+        for (int size = start; size <= endRapido; size *= 10) {
+    for (int j = 0; j < 5; j++) {
+        ListaSimpleConCola<Integer> lista = new ListaSimpleConCola<>();
+        exec(size, "pushBack_tail", lista::pushBack);
+    }
+}
+        for (int size = start; size <= endRapido; size *= 10) {
+    for (int j = 0; j < 5; j++) {
+        MyStack<Integer> stack = new MyStack<>();
+        exec(size, "stack_push", stack::push);
+    }
+}
+    for (int size = start; size <= endRapido; size *= 10) {
+    for (int j = 0; j < 5; j++) {
+        MyQueue<Integer> queue = new MyQueue<>();
+        exec(size, "queue_enqueue", queue::enqueue);
+    }
+}
     }
 }
