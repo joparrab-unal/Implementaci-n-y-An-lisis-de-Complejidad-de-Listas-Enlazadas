bbea25301a7478b184b73b55c0f68b25c37a0b98
joparrab-unal <joparrab@unal.edu.co>
2026-03-22 22:03:23 -0500

    Update ListaSimpleConCola.java

--- ListaSimpleConCola.java
+++ ListaSimpleConCola.java
@@ -1,1 +1,42 @@
-
+public class ListaSimpleConCola<T> {
+  private Nodo<T> head;
+  private Nodo<T> tail;
+public ListaSimpleConCola(){
+  head= null;
+  tail = null;
+}
+  public void pushFront(T x){
+    Nodo<T> nuevo = new Nodo(x);
+    nuevo.siguiente = head;
+    head = nuevo;
+    if(tail == null){
+      tail = head;
+    }
+  }
+  public T popFront(){
+    if(head == null) return null;
+    T val = head.valor;
+    head = head.siguiente;
+    if(head == null){
+      tail = null;
+    }
+    return val;
+  }
+  public T popBack(){
+    if(head == tail) {
+      if (head == tail){
+        T val = head.valor;
+        head = tail = null;
+        return val;
+      }
+      Nodo<T> actual = head;
+      while (actual.siguiente != tail){
+        actual = actual.siguiente;
+      }
+      T val = tail.valor;
+      tail = actual;
+      tail.siguiente = null;
+      return val;
+    }
+  }
+      
