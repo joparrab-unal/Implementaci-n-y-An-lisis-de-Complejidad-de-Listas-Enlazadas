6b00650a87f00d4c94dcab7e3725cbd82526a797
joparrab-unal <joparrab@unal.edu.co>
2026-03-22 21:54:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+import java.time.Instant;
+import java.time.Duration;
 
+public class Main {
+
+    public static void exec(int size, String method, Operation operation) {
+        Instant start = Instant.now();        
+        
+        for (int i =0; i<size; i++)
+            operation.apply(i);
+
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        System.out.printf("Se ejecutó %s de %d elementos en: %d milisegundos\n", method, size, timeElapsed);
+    }
+
+    public static void main(String[] args) {
+        final int start = 100;
+        final int end = 10000;
+        
+        for (int size = start; size <= end; size *= 10)
+            for (int i =0; i<5; i++){
+                ListaSimple<Integer> lista = new ListaSimple<>();
+                exec(size, "pushFront", lista::pushFront;
+            }
+    
+    }
+}
